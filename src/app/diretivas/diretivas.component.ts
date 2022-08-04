c77f9261e03ce43b9b6d646fbab169ce0a3ee682
Tiago Lima <james.lima.devops@gmail.com>
2022-08-03 23:05:49 -0300

    diretivas de estilo

--- src/app/diretivas/diretivas.component.ts
+++ src/app/diretivas/diretivas.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasComponent implements OnInit {
 
+  // ____________________ exemplo 2:
+  size = 25;
+  font = 'Arial';
+  color = 'darkblue';
+
+  // ____________________ exemplo 3:
+  classes = ["green-title", "small-title"];
+
+  // ____________________ exemplo 3:
+  underline = 'underlined-title';
+
+
   constructor() { }
 
   ngOnInit(): void {
